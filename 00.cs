91d5a777682c9da3b0fe45d5b2186cb0f6b65bff
23MacielValentino <valentino.macielet32@gmail.com>
2026-03-19 11:23:30 -0300

    Add files via upload

---
+++ 00.cs
@@ -0,0 +1,231 @@
+﻿int a1 = 25;
+int b1 = 30;
+if (a1 > b1)
+{
+    Console.WriteLine("1) V");
+}else
+{
+    Console.WriteLine("1) F");
+}
+
+int a2 = 14;
+int b2 = 14;
+if (a2 <= b2)
+{
+    Console.WriteLine("2) V");
+}
+else
+{
+    Console.WriteLine("2) F");
+}
+
+int a3 = 0;
+int b3 = 0;
+if (a3 = b3)
+{
+    Console.WriteLine("3) V");
+}
+else
+{
+    Console.WriteLine("3) F");
+}
+
+int a4 = 10;
+int b4 = 10;
+if (a4!= b4)
+{
+    Console.WriteLine("4) V");
+}
+else
+{
+    Console.WriteLine("4) F");
+}
+
+int a5 = 8;
+int b5 = 3;
+if (a5 >= b5)
+{
+    Console.WriteLine("5) V");
+}
+else
+{
+    Console.WriteLine("5) F");
+}
+
+int a6 = 15;
+int b6 = 15;
+if (a6 < b6)
+{
+    Console.WriteLine("6) V");
+}
+else
+{
+    Console.WriteLine("6) F");
+}
+
+int a7 = 100;
+int b7 = 50;
+if (a7!= b7)
+{
+    Console.WriteLine("7) V");
+}
+else
+{
+    Console.WriteLine("7) F");
+}
+
+int a8 = 7;
+int b8 = 7.0;
+if (a8 = b8)
+{
+    Console.WriteLine("8) V");
+}
+else
+{
+    Console.WriteLine("8) F");
+}
+
+int a9 = 0.5;
+int b9 = 0;
+if (a9 > b9)
+{
+    Console.WriteLine("9) V");
+}
+else
+{
+    Console.WriteLine("9) F");
+}
+
+int a10 = -5;
+int b10 = -2;
+if (a10 < b10)
+{
+    Console.WriteLine("10) V");
+}
+else
+{
+    Console.WriteLine("10) F");
+}
+
+int a11 = 5>3;
+int b11 = 10<20;
+if ((a11) && (b11))
+{
+    Console.WriteLine("11) V");
+}
+else
+{
+    Console.WriteLine("11) F");
+}
+
+int a12 = 8 == 10;
+int b12 = 5!= 2;
+if ((a12) || (b12))
+{
+    Console.WriteLine("12) V");
+}
+else
+{
+    Console.WriteLine("12) F");
+}
+
+int a13 = 10;
+int b13 = 10;
+if ( !(a13 = b13))
+{
+    Console.WriteLine("13) V");
+}
+else
+{
+    Console.WriteLine("13) F");
+}
+
+int a14 = 15 > 10;
+int b14 = 5 > 10;
+if ((a14) &&! (b14))
+{
+    Console.WriteLine("14) V");
+}
+else
+{
+    Console.WriteLine("14) F");
+}
+
+int a15 = 12 > 10;
+int b15 = 10 < 8;
+if ((a15) || (b15))
+{
+    Console.WriteLine("15) V");
+}
+else
+{
+    Console.WriteLine("15) F");
+}
+
+int a16 = 35>30;
+string b16 = "F";
+if ((a16) && (b16="V"))
+{
+    Console.WriteLine("16) V");
+}
+else
+{
+    Console.WriteLine("11) F");
+}
+
+int a17 = 16;
+if (a17 >= 18)
+{
+    Console.WriteLine("17) V");
+}
+else
+{
+    Console.WriteLine("17) F");
+}
+
+int a18 = 10/3=3;
+int b18 = 10%3=1;
+if ((a18) && (b18))
+{
+    Console.WriteLine("11) V");
+}
+else
+{
+    Console.WriteLine("11) F");
+}
+
+string a19 = "Ana";
+string b19 = "Juan";
+if (a19=!"Ana" || b19="Ana")
+{
+    Console.WriteLine("11) V");
+}
+else
+{
+    Console.WriteLine("11) F");
+}
+
+int a20 = 20>15;
+int b20 = 5=5;
+if ((a20) && (b20))
+{
+    Console.WriteLine("20) V");
+}
+else
+{
+    Console.WriteLine("11) F");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
